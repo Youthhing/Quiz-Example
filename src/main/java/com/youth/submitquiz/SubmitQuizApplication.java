0af71cb6f23857e77c4e4a217cf4ae4bb6e1265c
youth <boysoeng@naver.com>
2024-06-27 17:28:00 +0900

    Chore: initial setting

---
+++ src/main/java/com/youth/submitquiz/SubmitQuizApplication.java
@@ -0,0 +1,13 @@
+package com.youth.submitquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmitQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubmitQuizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youth/submitquiz/SubmitQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youth.submitquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmitQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
