67e8564c7c2059f582b7be8da895148d1d944c75
youth <boysoeng@naver.com>
2024-06-30 18:42:55 +0900

    feat: 요청 순서를 확인하는 Redis Repository 구현

---
+++ src/main/java/com/youth/submitquiz/redis/TicketNumberRepository.java
@@ -0,0 +1,19 @@
+package com.youth.submitquiz.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class TicketNumberRepository {
+
+    private static final String KEY_PREFIX = "$quiz_";
+    private final RedisTemplate<String , String > redisTemplate;
+
+    public Long increment(Long quizId){
+        String key = KEY_PREFIX + quizId;
+        return redisTemplate.opsForValue()
+                .increment(key);
+    }
+}
