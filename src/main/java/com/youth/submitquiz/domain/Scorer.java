5463bc8e7f5459a0cb8fdfb0f962cda4603154af
youth <boysoeng@naver.com>
2024-07-05 10:47:41 +0900

    feat: 득점자 테이블 수정 및 업데이트 로직 추가

--- src/main/java/com/youth/submitquiz/domain/Scorer.java
+++ src/main/java/com/youth/submitquiz/domain/Scorer.java
@@ -1,5 +1,6 @@
 package com.youth.submitquiz.domain;
 
+import com.youth.submitquiz.common.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,20 +14,29 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Scorer {
+public class Scorer extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "quiz_id", nullable = false)
+    @Column(name = "quiz_id", nullable = false, unique = true)
     private Long quizId;
 
     @Column(name = "member_id", nullable = false)
     private Long memberId;
 
+    @Column(name = "ticket_number", nullable = false)
+    private Long ticketNumber;
+
     @Builder
-    public Scorer(final Long quizId, Long memberId) {
+    public Scorer(final Long quizId, Long memberId, Long ticketNumber) {
         this.quizId = quizId;
         this.memberId = memberId;
+        this.ticketNumber = ticketNumber;
+    }
+
+    public void updateScorer(Long memberId, Long ticketNumber) {
+        this.memberId = memberId;
+        this.ticketNumber = ticketNumber;
     }
 }
